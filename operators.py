e3b59e5a5819b205ba013b09eb34b434c0150354
André <algc321@gmail.com>
2020-09-13 17:42:48 -0300

    checking for nan values in interactions

--- operators.py
+++ operators.py
@@ -4,7 +4,16 @@ import pandas as pd
 op = {"+": operator.add, "-": operator.sub}
 tabela = pd.read_csv("interactions.csv", index_col=0)
 soma = tabela.loc["FRP", "FNN"]
-for i in range (len(tabela.index)):
-    print(tabela.index[i])
+dim = len(tabela.index)
+for i in range (dim):
+    for l in range(dim):
+        a = tabela.index[i]
+        b = tabela.index[l]
+        operation = tabela.loc[a,b]
+        if pd.isnull(operation):
+            print(a,b)
+        #     pass
+        # else:
+        #     print(op[operation](1,2))
 #print(op[soma](1,2))
 #print(ops['+'](1,2))
