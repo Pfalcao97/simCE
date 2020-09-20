a4e5ffb5b53cd9db93a04b137dae2f9841dc7171
André <algc321@gmail.com>
2020-09-20 19:51:01 -0300

    update main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# IMPORTAÇÃO DOS OBJETOS CRIADOS
+# IMPORTAÇÃO DOS OBJETOS CRIADOS #
 
 from screenManager import Screen, Button
 from simOBJ import Inputbox
@@ -6,7 +6,7 @@ from screen_sim import Bolinha, Timer
 import pygame as pg 
 import numpy as np
 
-# VARIÁVEIS GLOBAIS
+# VARIÁVEIS GLOBAIS #
 # Aqui o Pygame é iniciado e são definidas todas as variáveis utilizadas como:
 # altura e largura da janela, telas (menu de parâmetros, simulação e exibição dos resultados),
 # input boxes para que o usuário possa inserir valores, botões, timer, etc.
@@ -16,11 +16,21 @@ pg.font.init()
 w_w = 1024
 w_h = 768
 done = False
-paramScreen = Screen("Valores", w_w, w_h) 
-simScreen = Screen("Simulação", w_w, w_h)
-win = paramScreen.makeCurrent()
 colours = {"white": (255,255,255), "black": (0,0,0), "shadow": (90,90,90), "bg": (220,220,220), "red": (255,0,0), "green": (0,255,0), "blue": (0,0,255)}
 
+# Janelas
+paramScreen = Screen("Valores", w_w, w_h, fill=colours['bg']) 
+simScreen = Screen("Simulação", w_w, w_h, fill=colours['bg'])
+win = paramScreen.makeCurrent() # Tela atual
+
+# Botões
+bStart = Button(775,665,150,50,colours['blue'],colours['blue'],None,40,colours['black'],"Start")
+bReturn = Button(775,665,150,50,colours['red'],colours['red'],None,40,colours['black'],"Return")
+bResults = Button(775,665,80,32,colours['green'],colours['green'],None,40,colours['black'],"Results")
+
+# REFERENTA À TELA DE PARÂMETROS #
+
+# Parâmetros para tela de valores
 FNN = Inputbox("FNN",100,100,50,32)
 FRP = Inputbox("FRP",100,150,50,32)
 SCD = Inputbox("SCD",100,200,50,32)
@@ -41,9 +51,37 @@ CSU = Inputbox("CSU",600,450,50,32)
 # Armazenamento dos parâmetros numa lista
 input_boxes = [FNN, FRP, SCD, SCE, SHI, ERR, ENR, EPE, PNS, PRS, PEX, PRM, PMN, PLG, SPP, CSU]
 exportValues = np.zeros(len(input_boxes)) # Lista com os valores de cada parâmetros para sem exportado
-bStart = Button(775,665,150,50,colours['blue'],colours['blue'],None,40,colours['black'],"Start")
-bReturn = Button(775,665,150,50,colours['red'],colours['red'],None,40,colours['black'],"Return")
-bResults = Button(775,665,80,32,colours['green'],colours['green'],None,40,colours['black'],"Results")
+
+# REFERENTE À TELA DE SIMULAÇÃO #
+# Timer
+clock = pg.time.Clock()
+timer = Timer(10,850,50)
+pg.time.set_timer(pg.USEREVENT+1,1000)
+timer_event = pg.USEREVENT+1
+
+# Elementos
+fnn = Bolinha('FNN',450,130)
+frp = Bolinha('FRP',560,130)
+pex = Bolinha('PEX',250,350)
+prm = Bolinha('PRM',380,350)
+pmn = Bolinha('PMN',510,350)
+plg = Bolinha('PLG',630,350)
+csu = Bolinha('CSU',760,350)
+scd = Bolinha('SCD',910,300)
+sce = Bolinha('SCE',910,400)
+shi = Bolinha('SHI',910,500)
+err = Bolinha('ERR',100,300)
+enr = Bolinha('ENR',100,400)
+epe = Bolinha('EPE',100,500)
+pns = Bolinha('PNS',250,660)
+prs = Bolinha('PRS',380,660)
+spp = Bolinha('SPP',510,520)
+
+# Armazenamento dos elementos numa lista
+elements = [fnn,frp,pex,prm,pmn,plg,csu,scd,sce,shi,err,enr,epe,pns,prs,spp]
+
+
+# Valores finais
 values = {}
 
 # LOOP PRINCIPAL DA APLICAÇÃO
@@ -84,10 +122,21 @@ while not done:
 
     # INÍCIO DA TELA DE SIMULAÇÃO
     elif simScreen.checkUpdate():
+        clock.tick(30)
+        timer.draw(simScreen.screen)
+        bReturn.showButton(simScreen.screen)
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 done = True
-        bReturn.showButton(simScreen.screen)
+            if event.type == timer_event:
+                timer.time -= 1
+                if timer.time == 0:
+                    print("cabo a graça")
+                timer.update()
+        for element in elements:
+            element.draw(simScreen.screen)
+        
+        
         if bReturn.focusCheck(mouse_pos, mouse_click):
             win = paramScreen.makeCurrent()
             simScreen.endCurrent()
--- screen_sim.py
+++ screen_sim.py
@@ -12,62 +12,66 @@ white = (255, 255, 255)
 black = (0,0,0) 
 shadow = (90,90,90)
 bg = (220,220,220)
-dic = {'FNN': 10.0, 'FRP': 1.0, 'SCD': -0.5, 'SCE': 0.0, 'SHI': 0.0, 'ERR': 1.0, 'ENR': 0.0, 'EPE': 0.005, 'PNS': 0.0, 'PRS': 0.0, 'PEX': 0.0, 'PRM': 0.0, 'PMN': 0.0, 'PLG': 0.0, 'SPP': 0.0, 'CSU': 0.0}
+values = {'FNN': 10.0, 'FRP': 1.0, 'SCD': -0.5, 'SCE': 0.0, 'SHI': 0.0, 'ERR': 1.0, 'ENR': 0.0, 'EPE': 0.005, 'PNS': 0.0, 'PRS': 0.0, 'PEX': 0.0, 'PRM': 0.0, 'PMN': 0.0, 'PLG': 0.0, 'SPP': 0.0, 'CSU': 0.0}
 
 class Bolinha:
-    def __init__(self, name, screen, x, y):
+    def __init__(self, name, x, y):
         self.name = name
         self.x = x
         self.y = y
+        #self.values = values
         self.screen = screen
         self.name_surface = FONT.render(self.name,True,black)
+        self.size = 15+int(values[self.name]*1)
 
-    def draw(self):
-        size = 15+int(dic[self.name]*1)
-        pg.gfxdraw.filled_circle(screen,self.x+2, self.y+2,size,shadow)
-        pg.gfxdraw.filled_circle(screen,self.x, self.y,size,white)
-        pg.gfxdraw.aacircle(screen,self.x, self.y,size,black)
+    def draw(self, screen):
+        pg.gfxdraw.filled_circle(self.screen,self.x+2, self.y+2,self.size,shadow)
+        pg.gfxdraw.filled_circle(self.screen,self.x, self.y,self.size,white)
+        pg.gfxdraw.aacircle(self.screen,self.x, self.y,self.size,black)
         screen.blit(self.name_surface, (self.x-int(FONT.size(self.name)[0]/2), self.y-int(FONT.size(self.name)[1]/2)))
 
+    #def updateSize(self):
+
+
 class Timer:
-    def __init__(self, screen, time, x, y):
+    def __init__(self, time, x, y):
         self.screen = screen
         self.time = time
         self.x = x
         self.y = y
         self.counter_surface = FONT.render(str(self.time), True, black)
-        self.timer_event = pg.USEREVENT+1
         self.timer_txt = FONT.render("Timer: ", True, black)
         
-    def handle_event(self,event):
-        if event.type == self.timer_event:
-            self.time -= 1
-            self.counter_surface = FONT.render(str(self.time), True, black)
+    def update(self):
+        # if event.type == self.timer_event:
+        #     self.time -= 1
+        self.counter_surface = FONT.render(str(self.time), True, black)
     
     def draw(self, screen):
         screen.blit(self.counter_surface, (self.x,self.y))
         screen.blit(self.timer_txt, (self.x-60,self.y))
 
 clock = pg.time.Clock()
-fnn = Bolinha('FNN',screen,450,130)
-frp = Bolinha('FRP',screen,560,130)
-pex = Bolinha('PEX',screen,250,350)
-prm = Bolinha('PRM',screen,380,350)
-pmn = Bolinha('PMN',screen,510,350)
-plg = Bolinha('PLG',screen,630,350)
-csu = Bolinha('CSU',screen,760,350)
-scd = Bolinha('SCD',screen,910,300)
-sce = Bolinha('SCE',screen,910,400)
-shi = Bolinha('SHI',screen,910,500)
-err = Bolinha('ERR',screen,100,300)
-enr = Bolinha('ENR',screen,100,400)
-epe = Bolinha('EPE',screen,100,500)
-pns = Bolinha('PNS',screen,250,660)
-prs = Bolinha('PRS',screen,380,660)
-spp = Bolinha('SPP',screen,510,520)
+fnn = Bolinha('FNN',450,130)
+frp = Bolinha('FRP',560,130)
+pex = Bolinha('PEX',250,350)
+prm = Bolinha('PRM',380,350)
+pmn = Bolinha('PMN',510,350)
+plg = Bolinha('PLG',630,350)
+csu = Bolinha('CSU',760,350)
+scd = Bolinha('SCD',910,300)
+sce = Bolinha('SCE',910,400)
+shi = Bolinha('SHI',910,500)
+err = Bolinha('ERR',100,300)
+enr = Bolinha('ENR',100,400)
+epe = Bolinha('EPE',100,500)
+pns = Bolinha('PNS',250,660)
+prs = Bolinha('PRS',380,660)
+spp = Bolinha('SPP',510,520)
 
-timer = Timer(screen,10,850,50)
+timer = Timer(10,850,50)
 pg.time.set_timer(pg.USEREVENT+1,1000)
+timer_event = pg.USEREVENT+1
 elements = [fnn,frp,pex,prm,pmn,plg,csu,scd,sce,shi,err,enr,epe,pns,prs,spp]
 def main():
     done = False
@@ -78,12 +82,14 @@ def main():
         timer.draw(screen)
         #screen.blit(image, (0, 0))
         for element in elements:
-            element.draw()
+            element.draw(screen)
         #mousepos = pg.mouse.get_pos()
         for event in pg.event.get():
                 if event.type == pg.QUIT:
                     done = True
-                timer.handle_event(event)
+                if event.type == timer_event:
+                     timer.time -= 1
+                timer.update()
         pg.display.flip()
         #print(mousepos[1],mousepos[0])
 if __name__ == "__main__":
